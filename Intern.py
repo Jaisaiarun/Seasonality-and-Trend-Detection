ed00a10431b5612599b31d18d811aa8da342780a
Jaisaiarun <48372623+Jaisaiarun@users.noreply.github.com>
2021-09-07 11:00:24 +0530

    unkown

--- Intern.py
+++ Intern.py
@@ -2,70 +2,70 @@
 import pandas as pd
 import datetime
 import numpy as np
+from statistics import mean
 from matplotlib import pyplot as plt
-import matplotlib
-from sklearn.gaussian_process.kernels import ConstantKernel, RBF
+from sklearn.gaussian_process.kernels import ConstantKernel, RBF ,WhiteKernel ,ExpSineSquared , RationalQuadratic
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
-#%%
+import seaborn as sns
+sns.set_palette(palette='deep')
+sns_c = sns.color_palette(palette='deep')
+
 def Data_Engineering(df):
     df=df.T
     df.columns=['ds','y']
     df.ds = pd.to_datetime(df.ds.apply(str).add('-0'), format='%Y%W-%w')
+    plt.figure()
+    plt.plot(df.ds,df.y)
+    plt.plot(df.ds,df.y, 'r.', markersize=5, label='Observations')
+    plt.xlabel('Week')
+    plt.xticks(rotation=90)
+    plt.ylabel('total transportation volume of the week')
+    plt.title('Given Value')
+    return df
+
+#%%
+def Seasonality_and_Trend_Detection(df,kernel,data_points=6):
+    gp = GaussianProcessRegressor(kernel=kernel,n_restarts_optimizer=9)
+    x = np.array(list(range(len(df)))).reshape(-1,1)
+    y=df.y.to_numpy().reshape(-1, 1)
+    gp.fit(x, y)
+    for i in range(data_points):
+        t=df["ds"].iloc[-1]+datetime.timedelta(weeks = 1)
+        df = df.append({'ds':t,'y':''},ignore_index=True)
+    X = np.array(list(range(len(df)))).reshape(-1,1)
+    y_pred, sigma = gp.predict(X, return_std=True)
+    plt.figure()
+#    plt.plot(x, y, 'r:', label='Actual')
+#    plt.plot(x, y, 'r.', markersize=10, label='Observations')
+    plt.plot(X, y_pred, 'r-', label='Prediction')
+    plt.legend(loc='upper left')
+    plt.xlabel('Week')
+    plt.ylabel('Total transportation volume of the week')
+    plt.title('Predicted Value')
+    plt.legend(loc='upper left')
+    print(y_pred)
+    df.y=y_pred
     return df
 #%%
-def f(x):    
-        return x * np.sin(x)
-#%%
-def Seasonality_and_Trend_Detection(df,data_points=6):
-    
-    return df
+k0 = WhiteKernel(noise_level=0.3**2,noise_level_bounds=(0.1**2,0.5**2))
+k1 = ConstantKernel(constant_value=2)*ExpSineSquared(length_scale=1.0,periodicity=40)
+k2 = ConstantKernel(constant_value=100, constant_value_bounds=(1, 500))*RationalQuadratic(alpha=50.0, length_scale=500)
+k3 = ConstantKernel(constant_value=1)*ExpSineSquared(length_scale=1.0,periodicity=12)
+kernel_2=k0+k1+k2+k3
+k0 = WhiteKernel(noise_level=0.5)
+k1 = ConstantKernel(constant_value=2,constant_value_bounds=(0.0, 1.0)) * \
+ExpSineSquared(length_scale=1.0, periodicity=40, periodicity_bounds=(35, 45))
+k2 = RBF(length_scale=1)  
+kernel  = k0 + k1 + k2
+kernel = ConstantKernel(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
 #%%
 df = pd.read_csv('sample.csv', header=None)
-#df=df.T
-#df.columns=['ds','y']
-#df.ds = pd.to_datetime(df.ds.apply(str).add('-0'), format='%Y%W-%w')
 df=Data_Engineering(df)
-print(df)
-#%%
-#print(np.array(lis(df)))
-#%%
-#plt.plot(list(range(len(df))),df.y)
-#dates = matplotlib.dates.date2num(df.ds)
-plt.plot(df.ds,df.y)
-plt.xticks(rotation=90) #turn x label 90
-plt.show()
-#print(df.ds)
-#%%
-kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
-gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=9)
-#X = np.atleast_2d([1., 3., 5., 6., 7., 8.]).T
-#x = np.atleast_2d(np.linspace(0, 10, 1000)).T
-x = np.array(list(range(len(df)))).reshape(-1,1)
-#x = df.ds.to_numpy().reshape(-1,1)
-#X=x
+#print(df)
 
-# Observations
-#y = f(X).ravel()
-y=df.y.to_numpy().reshape(-1, 1)
-gp.fit(X, y)
-# Make the prediction on the meshed x-axis (ask for MSE as well)
-#y_pred, sigma = gp.predict(df.y.to_numpy().reshape(-1, 1), return_std=True)
-y_pred, sigma = gp.predict(x, return_std=True)
+df_1=Seasonality_and_Trend_Detection(df,kernel,10)
+#print(df_1)
 
-#%%
-# Plot the function, the prediction and the 95% confidence interval based on
-# the MSE
-plt.figure()
-#plt.plot(x, f(x), 'r:', label=r'$f(x) = x\,\sin(x)$')
-plt.plot(X, y, 'r.', markersize=10, label='Observations')
-plt.plot(x, y_pred, 'b-', label='Prediction')
-plt.fill(np.concatenate([x, x[::-1]]),
-        np.concatenate([y_pred - 1.9600 * sigma,
-                        (y_pred + 1.9600 * sigma)[::-1]]),
-         alpha=.5, fc='b', ec='None' )
-plt.xlabel('$x$')
-plt.ylabel('$f(x)$')
-#plt.ylim(-10, 20)
-plt.legend(loc='upper left')
+
+
 
